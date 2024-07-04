f8523f439fdacbfc3510147591d8a06e36ba4340
Igor Augusto <igoraugustobz@gmail.com>
2024-07-04 18:27:40 -0300

    :lipstick: feat: Estilização do CSS

--- palmeiras-web/src/ui/components/header/Header.tsx
+++ palmeiras-web/src/ui/components/header/Header.tsx
@@ -1,32 +1,25 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
     <>
       <div className="mx-auto max-w-[1350px]">
-        <div className="flex justify-between items-center bg-palmeiras-green-light py-5 px-4">
+        <header className="flex justify-between items-center bg-[#03eb99] my-4 py-5 px-4 rounded">
           <div>
             <button>Logo</button>
           </div>
 
-          <div className="flex gap-x-4">
-            <button>Produtos Oficiais</button>
-            <button>Coleções</button>
-            <button>Para sócios</button>
-            <button>Notícias</button>
-          </div>
+          <nav className='flex gap-x-6'>
+            <a href="produtos">Novidades</a>
+            <a href="colecoes">Licenciados</a>
+            <a href="socios">Coleções</a>
+            <a href="https://futuro-site-de-noticias.com.br">Promoções</a>
+            <a href="promocoes">Para Sócios</a>
+            <a href="contato">Contato</a>
+          </nav>
 
-          <div className="flex-grow-[0.35] mx-4">
-            <input
-              type="text"
-              className="w-full p-0.5 rounded"
-              placeholder="Pesquisar..."
-            />
-          </div>
-
-          <div className="flex gap-x-4">
-            <button>Sign In</button>
-            <button>Sign Up</button>
-          </div>
-        </div>
+          <div className="flex gap-x-4">1 2 3 4</div>
+        </header>
       </div>
     </>
   );
