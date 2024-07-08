f3454d4f4f72534b89034cbd1ea101762aa11565
Igor Augusto <igoraugustobz@gmail.com>
2024-07-07 22:01:36 -0300

    :sparkles: feat: Adicionada lista suspensa do componente ShoppingCart

--- palmeiras-web/src/components/layout/Header.tsx
+++ palmeiras-web/src/components/layout/Header.tsx
@@ -1,8 +1,11 @@
-import { User, ShoppingCart } from 'lucide-react';
+import { User, ShoppingCart, X } from 'lucide-react';
 import logo from '../../assets/images/logo-palmeiras.png';
 import SearchInput from '../common/SearchInput';
+import { useState } from 'react';
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <div className="mx-auto max-w-[80%]">
       <header className="flex justify-between items-center bg-white my-4 p-[18px]">
@@ -17,7 +20,13 @@ const Header = () => {
         <SearchInput className="hidden md:flex w-1/3 mx-4" />
 
         <nav className="flex gap-x-4">
-          <ShoppingCart color="#4b5563" size={24} className="cursor-pointer" />
+          <div onClick={() => setToggle(!toggle)}>
+            <ShoppingCart
+              color="#4b5563"
+              size={24}
+              className="cursor-pointer"
+            />
+          </div>
           <User color="#4b5563" size={24} className="cursor-pointer" />
         </nav>
       </header>
@@ -25,6 +34,19 @@ const Header = () => {
       <div className="md:hidden mb-6">
         <SearchInput className="w-full mx-4" />
       </div>
+
+      {toggle && (
+        <div className="z-10 top-0 right-0 w-full sm:w-[40%] xl:w-1/3 h-full absolute bg-white sm:rounded-s-[40px] sm:shadow-black sm:shadow-2xl p-8">
+          <div className="flex justify-between items-center">
+            <h2 className="text-xl font-bold">Seu carrinho | {0} Itens</h2>
+            <span className=""></span>
+            <div onClick={() => setToggle(!toggle)}>
+              <X className="hover:bg-green-200 rounded-full cursor-pointer" />
+            </div>
+          </div>
+          <div className="mt-2 mb-6 border-[1px] "></div>
+        </div>
+      )}
     </div>
   );
 };
