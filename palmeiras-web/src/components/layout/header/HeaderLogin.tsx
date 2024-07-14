62a0e4dfe3e36ad9f6ec188a46c37bc64574d26c
Igor Augusto <igoraugustobz@gmail.com>
2024-07-14 20:24:23 -0300

    :sparkles: feat: Responsividade aperfeiçoada

--- palmeiras-web/src/components/layout/header/HeaderLogin.tsx
+++ palmeiras-web/src/components/layout/header/HeaderLogin.tsx
@@ -1,11 +1,20 @@
+import { ChevronLeft } from 'lucide-react';
 import logo from '../../../assets/images/logo.png';
+import { Link } from 'react-router-dom';
 
 const HeaderLogin = () => {
   return (
     <>
-      <header className="sticky top-0 z-10 backdrop-blur-lg bg-opacity-80">
-        <nav className="flex justify-center py-5 px-12">
-          <img src={logo} alt="Logotipo" className="w-16 sm:w-[74px]" />
+      <header className="sticky top-0 z-10 backdrop-blur-lg bg-opacity-80 py-5">
+        <nav className="flex justify-between items-center py-5 px-8 relative">
+          <Link to="/" className="flex items-center text-lg">
+            <ChevronLeft />
+            Voltar
+          </Link>
+
+          <div className="absolute left-1/2 transform -translate-x-1/2">
+            <img src={logo} alt="Logotipo" className="w-16 sm:w-[74px]" />
+          </div>
         </nav>
       </header>
     </>
--- palmeiras-web/src/components/layout/main/MainHome.tsx
+++ palmeiras-web/src/components/layout/main/MainHome.tsx
@@ -3,7 +3,7 @@ const Main = () => {
     <>
       <main>
         <div className="hidden sm:block image sm:h-[550px] 3xl:h-[700px] 4xl:h-[900px] w-full"></div>
-        <div className="sm:hidden image-mobile h-[700px] w-full"></div>
+        <div className="sm:hidden image-mobile h-[550px] w-full"></div>
       </main>
     </>
   );
