4b79f5017eb8d87a973bd8363321c7c548a38910
Igor Augusto <igoraugustobz@gmail.com>
2024-08-10 20:20:25 -0300

    :lipstick: feat: Estilização do componente adicionada

--- palmeiras-web/src/pages/Home/index.tsx
+++ palmeiras-web/src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import HomeFooter from '@/components/layout/footer/HomeFooter';
 import ProductCard from '../../components/product/ProductCard';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Link } from 'react-router-dom';
+import Button from '@/components/common/Button';
 
 type Product = {
   id: number;
@@ -130,14 +131,23 @@ const Home: React.FC = () => {
       </main>
 
       <section className="mt-20">
-        <div className="banner h-[550px] sm:h-[550px] 3xl:h-[700px] 4xl:h-[900px] w-full relative">
+        <div className="banner h-[400px] sm:h-[550px] 3xl:h-[700px] 4xl:h-[900px] w-full relative">
           <div
-            className="
-      absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
-      md:top-1/2 md:left-auto md:-right-10 lg:-right-20 md:transform md:-translate-y-1/2
-      bg-white w-[80%] sm:w-[60%] md:w-[30%]
-    "
-          ></div>
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
+      md:top-1/2 md:left-auto md:-right-10 lg:-right-20 md:transform md:-translate-y-1/2"
+          >
+            <div className="absolute inset-0 bg-white opacity-70 rounded-xl"></div>
+
+            <div className="relative w-full p-6">
+              <h2 className="font-semibold text-3xl sm:text-4xl max-w-[400px]">
+                Coleção casual Palmeiras Leisure
+              </h2>
+              <Button
+                classes="text-center text-white py-4 rounded-xl bg-green-900 mt-6 active:scale-[0.99] w-full sm:w-[55%]"
+                text="Conferir"
+              />
+            </div>
+          </div>
         </div>
       </section>
 
