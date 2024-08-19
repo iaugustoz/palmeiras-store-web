662fbe4db1737438080188e8d31ee99af1607e61
Igor Augusto <igoraugustobz@gmail.com>
2024-08-18 23:27:34 -0300

    :recycle: refactor: Estilo do Carrossel completamente alterado

--- palmeiras-web/src/components/product/ProductCard.tsx
+++ palmeiras-web/src/components/product/ProductCard.tsx
@@ -12,19 +12,27 @@ const ProductCard: React.FC<ProductCardProps> = ({
   image,
 }) => {
   return (
-    <div className="h-[450px] w-max-[300px] bg-white shadow shadow-gray-500 rounded-3xl p-5 my-5">
+    <div className="bg-white shadow-lg rounded-3xl p-5 my-5 hover:shadow-2xl transition-shadow duration-300 focus-within:shadow-2xl">
       <img
         src={image}
-        alt={productName}
-        className="w-full h-[60%] object-cover hover:scale-95"
+        alt={`Imagem do produto ${productName}`}
+        className="w-auto h-48 sm:h-64 md:h-72 object-cover rounded-t-2xl transition-transform duration-300 group-hover:scale-105"
       />
       <div className="flex flex-col mt-4">
         <h3 className="text-lg font-bold sm:max-w-[80%] max-w-full hover:underline">
           {productName}
         </h3>
-        <p className="text-gray-600 mt-2">{description}</p>
-        <div className="mt-4">
-          <span className="text-lg font-semibold">R${price}</span>
+        <p className="text-gray-600 mt-2 text-sm sm:text-base">{description}</p>
+        <div className="mt-4 flex items-center justify-between">
+          <span className="text-lg font-semibold text-green-600">
+            R${price}
+          </span>
+          <button
+            className="px-4 py-2 bg-green-600 text-white rounded-full text-sm font-medium hover:bg-green-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2"
+            aria-label={`Adicionar ${productName} ao carrinho`}
+          >
+            Adicionar ao Carrinho
+          </button>
         </div>
       </div>
     </div>
--- palmeiras-web/src/pages/Home/index.tsx
+++ palmeiras-web/src/pages/Home/index.tsx
@@ -1,68 +1,7 @@
 import HomeHeader from '@/components/layout/header/HomeHeader';
 import HomeFooter from '@/components/layout/footer/HomeFooter';
-import ProductCard from '../../components/product/ProductCard';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Link } from 'react-router-dom';
 import Button from '@/components/common/Button';
-
-type Product = {
-  id: number;
-  price: number;
-  description: string;
-  productName: string;
-  image: string;
-};
-
-const products: Product[] = [
-  {
-    id: 1,
-    price: 359.99,
-    description: 'Short description of the product.',
-    productName: 'Camisa I 24/25 Torcedor',
-    image:
-      'https://lojapalmeiras.vteximg.com.br/arquivos/ids/182880-1000-1000/_0051_777238_01.jpg?v=638434330594070000',
-  },
-  {
-    id: 2,
-    price: 359.99,
-    description: 'Short description of the product.',
-    productName: 'Camisa I 24/25 Jogador',
-    image:
-      'https://lojapalmeiras.vteximg.com.br/arquivos/ids/182880-1000-1000/_0051_777238_01.jpg?v=638434330594070000',
-  },
-  {
-    id: 3,
-    price: 299.99,
-    description: 'Short description of the product.',
-    productName: 'Camisa II 24/25 Torcedor',
-    image:
-      'https://lojapalmeiras.vteximg.com.br/arquivos/ids/182880-1000-1000/_0051_777238_01.jpg?v=638434330594070000',
-  },
-  {
-    id: 4,
-    price: 289.99,
-    description: 'Short description of the product.',
-    productName: 'Camisa II 24/25 Jogador',
-    image:
-      'https://lojapalmeiras.vteximg.com.br/arquivos/ids/182880-1000-1000/_0051_777238_01.jpg?v=638434330594070000',
-  },
-  {
-    id: 5,
-    price: 279.99,
-    description: 'Short description of the product.',
-    productName: 'Camisa III 24/25 Jogador',
-    image:
-      'https://lojapalmeiras.vteximg.com.br/arquivos/ids/182880-1000-1000/_0051_777238_01.jpg?v=638434330594070000',
-  },
-  {
-    id: 6,
-    price: 269.99,
-    description: 'Short description of the product.',
-    productName: 'Camisa III 24/25 Jogador',
-    image:
-      'https://lojapalmeiras.vteximg.com.br/arquivos/ids/182880-1000-1000/_0051_777238_01.jpg?v=638434330594070000',
-  },
-];
+import SwiperCustom from '@/components/common/SwiperCustom';
 
 const Home: React.FC = () => {
   return (
@@ -80,53 +19,7 @@ const Home: React.FC = () => {
             Coleções
           </h2>
 
-          <Swiper
-            pagination={{ clickable: true }}
-            spaceBetween={30}
-            slidesPerView={4.5}
-            slidesPerGroup={1}
-            keyboard={true}
-            autoplay={{ delay: 3000 }}
-            breakpoints={{
-              300: {
-                slidesPerView: 1.25,
-                spaceBetween: 10,
-                centeredSlides: true,
-              },
-              520: {
-                slidesPerView: 1.75,
-                spaceBetween: 15,
-              },
-              768: {
-                slidesPerView: 2.5,
-                spaceBetween: 20,
-              },
-              1100: {
-                slidesPerView: 3.5,
-              },
-              1536: {
-                slidesPerView: 4.5,
-                spaceBetween: 30,
-              },
-              2100: {
-                slidesPerView: 4.5,
-                spaceBetween: 40,
-              },
-            }}
-          >
-            {products.map((product) => (
-              <SwiperSlide key={product.id}>
-                <Link to={`/product/${product.id}`} target="_top">
-                  <ProductCard
-                    description={product.description}
-                    image={product.image}
-                    price={product.price}
-                    productName={product.productName}
-                  />
-                </Link>
-              </SwiperSlide>
-            ))}
-          </Swiper>
+          <SwiperCustom />
         </section>
       </main>
 
@@ -136,7 +29,7 @@ const Home: React.FC = () => {
             className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
       md:top-1/2 md:left-auto md:-right-10 lg:-right-20 md:transform md:-translate-y-1/2 w-[80%] sm:w-[60%] md:w-[30%]"
           >
-            <div className="absolute inset-0 bg-white opacity-85 rounded-xl"></div>
+            <div className="absolute inset-0 bg-white bg-opacity-85 rounded-xl"></div>
 
             <div className="relative w-full p-6">
               <h2 className="font-semibold text-3xl sm:text-4xl max-w-[400px]">
