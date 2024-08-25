3f6211ae4804dc343be7546db3ecf2c0e7464f21
Igor Augusto <igoraugustobz@gmail.com>
2024-08-25 11:35:06 -0300

    :sparkles: feat: Adição de Card no Banner

--- palmeiras-web/src/pages/Home/index.tsx
+++ palmeiras-web/src/pages/Home/index.tsx
@@ -28,7 +28,24 @@ const Home: React.FC = () => {
         </section>
 
         <section className="my-20">
-          <div className="banner-camisa-93 h-[400px] sm:h-[550px] 3xl:h-[700px] 4xl:h-[900px] w-full"></div>
+          <div className="banner-camisa-93 h-[400px] sm:h-[550px] 3xl:h-[700px] 4xl:h-[900px] w-full relative">
+            <div
+              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
+      md:top-1/2 md:left-auto md:-right-10 lg:-right-20 md:transform md:-translate-y-1/2 w-[80%] sm:w-[60%] md:w-[30%]"
+            >
+              <div className="absolute inset-0 bg-white bg-opacity-85 rounded-xl"></div>
+
+              <div className="relative w-full p-6">
+                <h2 className="font-semibold text-3xl sm:text-4xl max-w-[400px]">
+                  Palmeiras 1993 (Camisa Comemorativa)
+                </h2>
+                <Button
+                  classes="text-center text-white py-4 rounded-xl bg-green-900 mt-6 active:scale-[0.99] w-full sm:w-[55%]"
+                  text="Conferir"
+                />
+              </div>
+            </div>
+          </div>
         </section>
 
         <section className="mt-10">
